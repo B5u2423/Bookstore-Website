98a4706cb25c6bc0b278dd7bf5f0297029003981
B5u2423 <masterofyordle@gmail.com>
2025-10-06 18:01:53 +0700

    add customer login and register form

--- bookstore/src/fe/src/views/UserLoginView.vue
+++ bookstore/src/fe/src/views/UserLoginView.vue
@@ -12,6 +12,17 @@ const prop = {
 
 <template>
   <CommonLayout :prop="prop">
-    User Login View
+    <v-container width="30%">
+      <v-card>
+        <v-card-text>
+          <v-text-field label="Email"></v-text-field>
+          <v-text-field label="Password"></v-text-field>
+        </v-card-text>
+
+        <v-card-actions class="justify-center border">
+          <v-btn primary size="large" class="bg-red">Hello</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
   </CommonLayout>
 </template>
