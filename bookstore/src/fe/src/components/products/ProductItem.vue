0f952e859b8cd3f569518e25c8964339b4f8efb1
B5u2423 <masterofyordle@gmail.com>
2025-10-07 11:52:25 +0700

    change project structure; add new item grid and a item component

---
+++ bookstore/src/fe/src/components/products/ProductItem.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps(['product'])
+</script>
+
+<template>
+  <v-card class="ma-2" width="200" hover>
+    <v-img
+      :src="product.image"
+      height="250"
+      cover
+    ></v-img>
+    
+    <v-card-title>{{ product.title }}</v-card-title>
+    <v-card-subtitle>{{ product.author }}</v-card-subtitle>
+    <v-card-text>
+      From: <strong>{{ product.price }}</strong>
+    </v-card-text>
+  </v-card>
+</template>
--- bookstore/src/fe/src/views/LandingView.vue
+++ bookstore/src/fe/src/views/LandingView.vue
@@ -1,30 +1,31 @@
 <script setup>
 import CommonLayout from '@/components/CommonLayout.vue';
-import ProductSlider from '@/components/ProductSlider.vue';
+import ProductSlider from '@/components/products/ProductSlider.vue';
+import ProductGrid from '@/components/products/ProductGrid.vue';
+
 const products = [
-  {
-    title: 'The Giver',
-    author: 'Lois Lowry',
-    price: '$4.19',
-    image: 'https://example.com/giver.jpg'
-  },
-  {
-    title: 'The Silent Patient',
-    author: 'Alex Michaelides',
-    price: '$5.19',
-    image: 'https://example.com/silent.jpg'
-  },
-  {
-    title: 'Atomic Habits',
-    author: 'James Clear',
-    price: '$9.39',
-    image: 'https://example.com/habits.jpg'
-  }
+  { title: 'The Giver', author: 'Lois Lowry', price: '$4.19', image: 'https://example.com/giver.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'The Silent Patient', author: 'Alex Michaelides', price: '$5.19', image: 'https://example.com/silent.jpg' },
+  { title: 'Atomic Habits', author: 'James Clear', price: '$9.39', image: 'https://example.com/habits.jpg' }
 ]
 </script>
 
 <template>
   <CommonLayout>
     <ProductSlider :products="products" title="Bestsellers" subtitle="Something"/>
+    <ProductGrid :products="products" title="Showcase" subtitle="Show some books"/>
   </CommonLayout> 
 </template>
