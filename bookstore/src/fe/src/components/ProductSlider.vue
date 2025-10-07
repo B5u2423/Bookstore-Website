e48adb36a76aac8c3c878de44cdeeb50f93d234d
B5u2423 <masterofyordle@gmail.com>
2025-10-07 10:11:10 +0700

    add product slider

---
+++ bookstore/src/fe/src/components/ProductSlider.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+const props = defineProps(['title', 'subtitle', 'products'])
+</script>
+
+<template>
+  <v-container>
+    <h2>{{ title }}</h2>
+    <p>{{ subtitle }}</p>
+    <v-slide-group show-arrows>
+      <v-slide-group-item
+        v-for="(product, index) in products"
+        :key="index"
+      >
+        <v-card class="mx-2" width="200">
+          <v-img
+            :src="product.image"
+            height="250"
+            cover
+          ></v-img>
+          
+          <v-card-title>{{ product.title }}</v-card-title>
+          <v-card-subtitle>{{ product.author }}</v-card-subtitle>
+          <v-card-text>
+            From: <strong>{{ product.price }}</strong>
+          </v-card-text>
+        </v-card>
+      </v-slide-group-item>
+    </v-slide-group>
+  </v-container>
+</template>
--- bookstore/src/fe/src/views/LandingView.vue
+++ bookstore/src/fe/src/views/LandingView.vue
@@ -1,9 +1,30 @@
 <script setup>
 import CommonLayout from '@/components/CommonLayout.vue';
+import ProductSlider from '@/components/ProductSlider.vue';
+const products = [
+  {
+    title: 'The Giver',
+    author: 'Lois Lowry',
+    price: '$4.19',
+    image: 'https://example.com/giver.jpg'
+  },
+  {
+    title: 'The Silent Patient',
+    author: 'Alex Michaelides',
+    price: '$5.19',
+    image: 'https://example.com/silent.jpg'
+  },
+  {
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    price: '$9.39',
+    image: 'https://example.com/habits.jpg'
+  }
+]
 </script>
 
 <template>
   <CommonLayout>
-    <p>This is the landing page</p>
+    <ProductSlider :products="products" title="Bestsellers" subtitle="Something"/>
   </CommonLayout> 
 </template>
