2c8d3ef647d2fc85ebf05c536fa45f40940bc8b0
B5u2423 <masterofyordle@gmail.com>
2025-11-25 19:02:27 +0700

    Update Ecom Layout
    Add category sidebar for later.

--- frontend/src/layouts/EComLayout.vue
+++ frontend/src/layouts/EComLayout.vue
@@ -7,8 +7,8 @@ import FooterSection from '@/components/ecom/Footer.vue'
    <NavBar /> <v-main class="bg-light-green-lighten-5 mt-3"
     > <v-container class="bg-transparent"
       > <v-row
-        > <v-col sm="3"> <v-sheet elevation="1">This is the category side bar</v-sheet> </v-col> <v-col> <router-view></router-view> </v-col>
-        </v-row
+        > <v-col sm="3"> <v-sheet elevation="1">This is the category side bar</v-sheet> </v-col>
+        <v-col> <router-view></router-view> </v-col> </v-row
       > </v-container
     > </v-main
   > <FooterSection />
