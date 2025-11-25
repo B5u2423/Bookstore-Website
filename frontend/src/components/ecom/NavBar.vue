2caaa4aa027a31628e42f6a03bc256970ff7261a
B5u2423 <masterofyordle@gmail.com>
2025-11-25 16:55:43 +0700

    Update Navigation bar

--- frontend/src/components/ecom/NavBar.vue
+++ frontend/src/components/ecom/NavBar.vue
@@ -28,14 +28,31 @@ async function handleLogout() {
 </script>
 
 <template>
-  <v-app-bar density="default" class="px-16">
-    <v-toolbar-title @click="goToHomePage">Bookshop</v-toolbar-title>
-      <v-toolbar-items>
-        <v-btn @click="goToRegister">Đăng ký</v-btn>
-        <v-btn @click="goToLogin">Đăng nhập</v-btn>
-        <v-btn @click="goToCart" icon="mdi-cart"></v-btn>
-        <v-btn icon="mdi-bell"></v-btn>
-      </v-toolbar-items>
-  </v-app-bar>
+   <v-app-bar density="default" class="px-16"
+    > <v-app-bar-title class="display-inline cursor-pointer"
+      > <v-responsive max-width="333" max-height="39"
+        > <v-img
+          src="https://theme.hstatic.net/200000845405/1001223012/14/logo.png?v=471"
+          class="cursor-pointer"
+          @click="goToHomePage"
+        ></v-img
+        > </v-responsive
+      > </v-app-bar-title
+    > <v-text-field
+      max-width="666"
+      placeholder="Search"
+      variant="outlined"
+      rounded
+      density="comfortable"
+      color="deep-purple accent-4"
+      class="custom-search"
+      clearable
+      prepend-inner-icon="mdi-magnify"
+      hide-details
+    /> <template v-slot:append
+      > <v-btn @click="goToRegister">Đăng ký</v-btn> <v-btn @click="goToLogin">Đăng nhập</v-btn>
+      <v-btn @click="goToCart" icon="mdi-cart"></v-btn> <v-btn icon="mdi-bell"></v-btn> </template
+    > </v-app-bar
+  >
 </template>
 
