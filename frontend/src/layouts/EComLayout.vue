d7fd012254f50293f80d632efcf5551af6d6e854
B5u2423 <masterofyordle@gmail.com>
2025-11-25 17:26:31 +0700

    Update Ecom Layout

--- frontend/src/layouts/EComLayout.vue
+++ frontend/src/layouts/EComLayout.vue
@@ -4,6 +4,13 @@ import FooterSection from '@/components/ecom/Footer.vue'
 </script>
 
 <template>
-   <NavBar /> <v-main> <router-view></router-view> </v-main> <FooterSection />
+   <NavBar /> <v-main class="bg-light-green-lighten-5 mt-3"
+    > <v-container class="bg-transparent"
+      > <v-row
+        > <v-col sm="3"> <v-sheet elevation="1">This is the category side bar</v-sheet> </v-col> <v-col> <router-view></router-view> </v-col>
+        </v-row
+      > </v-container
+    > </v-main
+  > <FooterSection />
 </template>
 
