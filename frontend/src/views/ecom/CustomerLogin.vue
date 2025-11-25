bd549e31736233655c50cfed4e7995ea9869177a
B5u2423 <masterofyordle@gmail.com>
2025-11-25 19:01:37 +0700

    Update Customer Login view

--- frontend/src/views/ecom/CustomerLogin.vue
+++ frontend/src/views/ecom/CustomerLogin.vue
@@ -1,4 +1,36 @@
 <script setup></script>
 
-<template>Login</template>
+<template>
+   <v-sheet
+    class="d-flex align-center justify-center text-center mx-auto pa-5 mb-6"
+    elevation="4"
+    max-width="800"
+    width="100%"
+    rounded
+    >
+    <div>
+
+      <h2 class="text-h5 text-black">Đăng nhập</h2>
+
+      <div class="my-2">
+         <v-text-field
+          density="compact"
+          min-width="400"
+          label="Email"
+          variant="outlined"
+        ></v-text-field
+        > <v-text-field density="compact" label="Mật khẩu" variant="outlined"></v-text-field>
+      </div>
+       <v-btn color="green" class="mb-3" width="100%">ĐĂNG NHẬP</v-btn>
+      <p class="text-black"> Hoặc đăng nhập bằng </p>
+       <v-btn color="blue" class="ma-2 pa-2" prepend-icon="mdi-facebook">Facebook</v-btn> <v-btn
+        color="red"
+        class="ma-2 pa-2"
+        prepend-icon="mdi-google"
+        >Google</v-btn
+      >
+    </div>
+     </v-sheet
+  >
+</template>
 
