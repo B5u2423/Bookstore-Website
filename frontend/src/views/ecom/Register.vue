c83c5f73f702fdd55e171a28a0741342ce4d2d7c
B5u2423 <masterofyordle@gmail.com>
2025-11-26 01:00:05 +0700

    Update validation rules for customer register and login views

--- frontend/src/views/ecom/Register.vue
+++ frontend/src/views/ecom/Register.vue
@@ -1,9 +1,19 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const visible = ref(false)
+const visibleRetype = ref(false)
+
+const rules = {
+  required: (v) => !!v || 'Không được bỏ trống trường',
+  email: (v) => /[\w\d.-]{6,30}@[\w\d.-]+/.test(v) || 'Email không hợp lệ',
+}
+</script>
 
 <template>
 
   <v-sheet
-    class="d-flex align-center justify-center text-center mx-auto pa-5 mb-6"
+    class="d-flex align-center justify-center mx-auto pa-5 mb-6"
     elevation="4"
     max-width="800"
     width="100%"
@@ -12,11 +22,12 @@
 
     <div>
 
-      <h2 class="text-h5 text-black">Đăng ký tài khoản</h2>
+      <h2 class="text-h5 text-black text-center">Đăng ký tài khoản</h2>
 
       <div class="my-2">
 
         <v-text-field
+        class="my-3"
           density="compact"
           min-width="400"
           label="Họ"
@@ -24,27 +35,39 @@
         ></v-text-field>
 
         <v-text-field
+        class="my-3"
           density="compact"
           label="Tên"
           variant="outlined"
         ></v-text-field>
 
         <v-text-field
+        class="my-3"
           density="compact"
           label="Email"
           variant="outlined"
+          :rules="[rules.email, rules.required]"
         ></v-text-field>
 
         <v-text-field
+        class="my-3"
           density="compact"
           label="Mật khẩu"
           variant="outlined"
+          :rules="[rules.required]"
+          :type="visible ? 'text' : 'password'"
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          @click:append-inner="visible = !visible"
         ></v-text-field>
 
         <v-text-field
           density="compact"
           label="Nhập lại mật khẩu"
           variant="outlined"
+          :rules="[rules.required]"
+          :type="visibleRetype ? 'text' : 'password'"
+          :append-inner-icon="visibleRetype ? 'mdi-eye-off' : 'mdi-eye'"
+          @click:append-inner="visibleRetype = !visibleRetype"
         ></v-text-field>
 
       </div>
@@ -56,6 +79,8 @@
       >
          ĐĂNG KÝ
       </v-btn>
+
+      <div class="text-center">
 
       <p class="text-black"> Hoặc đăng nhập bằng</p>
 
@@ -75,6 +100,7 @@
          Google
       </v-btn>
 
+      </div>
     </div>
 
   </v-sheet>
