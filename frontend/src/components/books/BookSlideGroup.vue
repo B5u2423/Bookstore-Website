77f0a6eaca8b5852cd57d3457424755e86af2816
B5u2423 <masterofyordle@gmail.com>
2025-11-27 17:59:45 +0700

    Change name book slide group

---
+++ frontend/src/components/books/BookSlideGroup.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template> 
+<v-slide-group>
+  <v-slide-group-item>
+    <v-card v-for="n in 14">
+      <v-card-title> Hello</v-card-title>
+      <v-card-text>Thisis item</v-card-text>
+    </v-card>
+  </v-slide-group-item>
+</v-slide-group>
+</template>
+
