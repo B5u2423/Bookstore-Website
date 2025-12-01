840d1f5ae660181ee0b713450a41341d16e335ef
B5u2423 <masterofyordle@gmail.com>
2025-12-01 18:30:40 +0700

    Update CORS

--- bookstore/src/main/java/dev/vubl/bookstore/controllers/BookController.java
+++ bookstore/src/main/java/dev/vubl/bookstore/controllers/BookController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/books")
-@CrossOrigin(origins = "http://localhost:5713")
 @RequiredArgsConstructor
 public class BookController {
   private final BookService bookService;
