1e6dafa154fdbfb385ed544982f37b1405e835f3
B5u2423 <masterofyordle@gmail.com>
2025-12-05 21:33:37 +0700

    Update category data table

--- frontend/src/views/admin/Category.vue
+++ frontend/src/views/admin/Category.vue
@@ -1,4 +1,140 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+// table
+const headers = ref([
+  { title: 'ID', key: 'id', align: 'start' },
+  { title: 'Tên danh mục', key: 'categoryName', align: 'start' },
+  { title: 'Danh mục cha', key: 'parent', align: 'start' },
+  { title: 'Các danh mục con', key: 'children', align: 'start' },
+  { title: 'Thao tác', key: 'actions', align: 'end', sortable: false },
+])
+const itemsPerPage = ref(10)
+const loading = ref(false)
+const serverItems = ref([])
+const totalItems = ref(0)
 
-<template>Category</template>
+async function loadItems(params) {
+  
+}
+
+</script>
+
+<template>
+
+  <v-data-table-server
+    v-model:items-per-page="itemsPerPage"
+    :headers="headers"
+    :items="serverItems"
+    :items-length="totalItems"
+    :loading="loading"
+    item-value="title"
+    items-per-page-text="Số danh mục hiển thị"
+    @update:options="loadItems"
+  >
+
+    <template v-slot:top>
+
+      <v-toolbar flat>
+
+        <v-toolbar-title>
+
+          <v-icon
+            color="medium-emphasis"
+            icon="mdi-book-multiple"
+            size="x-small"
+            start
+          ></v-icon>
+          Thông tin danh mục
+        </v-toolbar-title>
+
+        <v-btn
+          class="me-2"
+          prepend-icon="mdi-plus"
+          rounded="lg"
+          text="Thêm danh mục"
+          variant="outlined"
+          @click="add"
+        ></v-btn>
+
+      </v-toolbar>
+
+    </template>
+
+    <!-- style the header -->
+
+    <template v-slot:headers="{ columns }">
+
+      <tr>
+
+        <template
+          v-for="column in columns"
+          :key="column.key"
+        >
+
+          <th>
+
+            <div class="d-flex align-center">
+
+              <span
+                class="me-2 cursor-pointer font-weight-bold"
+                v-text="column.title"
+              ></span>
+
+            </div>
+
+          </th>
+
+        </template>
+
+      </tr>
+
+    </template>
+
+    <!-- stylized book title as chips -->
+
+    <template v-slot:item.title="{ value }">
+
+      <v-chip
+        :text="value"
+        class="border-thin"
+        prepend-icon="mdi-book"
+        label
+      >
+
+        <template v-slot:prepend>
+
+          <v-icon color="medium-emphasis"></v-icon>
+
+        </template>
+
+      </v-chip>
+
+    </template>
+
+    <!-- action buttons -->
+
+    <template v-slot:item.actions="{ item }">
+
+      <div class="d-flex ga-2 justify-end">
+
+        <v-icon
+          color="medium-emphasis"
+          icon="mdi-pencil"
+          size="small"
+          @click="edit(item.id)"
+        ></v-icon>
+
+        <v-icon
+          color="medium-emphasis"
+          icon="mdi-delete"
+          size="small"
+          @click="confirm(item.id)"
+        ></v-icon>
+
+      </div>
+
+    </template>
+
+  </v-data-table-server>
+</template>
 
