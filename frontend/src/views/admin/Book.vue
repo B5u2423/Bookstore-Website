d02dafeba6924423f4efe4ee78511f0e4ac6d4e9
B5u2423 <masterofyordle@gmail.com>
2025-12-05 21:27:29 +0700

    Minor bug
    Id of new book will now be total item + 1 properly

--- frontend/src/views/admin/Book.vue
+++ frontend/src/views/admin/Book.vue
@@ -166,7 +166,7 @@ async function save() {
       console.error('Error adding new book')
     }
     // update the immediate view
-    formModel.value.id = serverItems.value.length + 1
+    formModel.value.id = totalItems.value++ 
     serverItems.value.push(formModel.value)
     totalItems.value++
   }
