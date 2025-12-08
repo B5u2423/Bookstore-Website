d4954be5b78a56d078b348d3c63e2c9a04e6ad26
B5u2423 <masterofyordle@gmail.com>
2025-12-08 18:14:36 +0700

    Add books for store page

--- frontend/src/views/ecom/CategoryShowcase.vue
+++ frontend/src/views/ecom/CategoryShowcase.vue
@@ -1,24 +1,36 @@
 <script setup>
 import { BookService } from '@/api/book-api';
+import VerticalBookCard from '@/components/books/VerticalBookCard.vue';
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute()
-const paginationLength = ref(1)
+const paginationLength = ref([])
+const books = ref([])
+
+async function fetchAllBooks() {
+  try {
+    const res = await BookService.fetchAllBooks({page: 0, size: 24})
+    paginationLength.value = res.page
+    books.value = res.content
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+onMounted(() => {
+  fetchAllBooks()
+})
+
 
 </script>
 
 <template>
   <v-row>
-    <v-col cols="12" md="3" v-for="i in 24">
-      <v-card title="Hello" subtitle="NOt hello">
-        <v-card-text>
-
-        {{ route.params.slug }}
-        </v-card-text>
-      </v-card>
+    <v-col cols="12" md="3" v-for="book in books">
+      <vertical-book-card :book="book" />
     </v-col>
   </v-row>
 
-  <v-pagination :length="paginationLength"></v-pagination>
+  <v-pagination :length="paginationLength.totalPages"></v-pagination>
 </template>
