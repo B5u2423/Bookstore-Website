1389cedd23e9782da15f00ca51dbd65ef1b19959
B5u2423 <masterofyordle@gmail.com>
2025-12-08 15:12:58 +0700

    Update category show case view

--- frontend/src/views/ecom/CategoryShowcase.vue
+++ frontend/src/views/ecom/CategoryShowcase.vue
@@ -1,7 +1,24 @@
 <script setup>
+import { BookService } from '@/api/book-api';
+import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute()
+const paginationLength = ref(1)
+
 </script>
 
-<template>{{ route.params.slug }}</template>
+<template>
+  <v-row>
+    <v-col cols="12" md="3" v-for="i in 24">
+      <v-card title="Hello" subtitle="NOt hello">
+        <v-card-text>
+
+        {{ route.params.slug }}
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+
+  <v-pagination :length="paginationLength"></v-pagination>
+</template>
