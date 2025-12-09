5c3f188ef993bb650d064c4e81ae32011da65402
B5u2423 <masterofyordle@gmail.com>
2025-12-09 22:23:51 +0700

    Update payment callback page

--- frontend/src/views/ecom/PaymentCallback.vue
+++ frontend/src/views/ecom/PaymentCallback.vue
@@ -1,6 +1,24 @@
-<script setup></script>
+<script setup>
+import { useRoute } from 'vue-router';
+
+
+  const route = useRoute()
+</script>
 
 <template>
-  Thanh toan thanh cong
-  <v-btn :to="{ name: 'landing' }">Quay lai trang chu</v-btn>
+  <v-card>
+
+    <v-card-text>
+
+  <template v-if="route.query.vnp_ResponseCode === '00'">
+    ok
+  </template>
+  <template v-else>
+    not ok
+  </template>
+    </v-card-text>
+  <v-card-actions class="justify-center align-center">
+    <v-btn>Tiếp tục mua sắm</v-btn>
+  </v-card-actions>
+  </v-card>
 </template>
