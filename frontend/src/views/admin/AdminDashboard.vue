2bcdb9f83604014bf4f5d4085a0d45783fed4d2f
B5u2423 <masterofyordle@gmail.com>
2025-12-22 15:06:01 +0700

    Wireframe the dashboard

--- frontend/src/views/admin/AdminDashboard.vue
+++ frontend/src/views/admin/AdminDashboard.vue
@@ -2,5 +2,27 @@
 
 </script>
 
-<template> Dashboard </template>
+<template>
+  <v-row>
+    <v-col cols="4">
+      <v-card>
+        <v-card-title>Số lượng sách được bày bán</v-card-title>
+        <v-card-text></v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="4">
+      <v-card>
+        <v-card-title>Số lượng đơn hàng</v-card-title>
+        <v-card-text></v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="4">
+      <v-card>
+        <v-card-title>Số lượng danh mục</v-card-title>
+        <v-card-text></v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+
+</template>
 
