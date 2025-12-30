55739fcfa0f417c05fe67ddd62ccc9129129f7d7
B5u2423 <masterofyordle@gmail.com>
2025-12-30 14:12:55 +0700

    Move success handler to a component

--- bookstore/src/main/java/dev/vubl/bookstore/configs/BookStoreConfig.java
+++ bookstore/src/main/java/dev/vubl/bookstore/configs/BookStoreConfig.java
@@ -4,8 +4,6 @@ import com.cloudinary.Cloudinary;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -21,22 +19,5 @@ public class BookStoreConfig {
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
-  }
-
-  @Bean
-  public AuthenticationSuccessHandler oAuth2SuccessHandler() {
-    return (request, response, authentication) -> {
-      OAuth2User user = (OAuth2User) authentication.getPrincipal();
-
-      String email = user.getAttribute("email");
-      String name = user.getAttribute("name");
-      String googleId = user.getAttribute("sub");
-
-      // 1. Find or create local user
-      System.out.println(name);
-      System.out.println(email);
-      System.out.println(googleId);
-      response.sendRedirect("https://example.com");
-    };
   }
 }
