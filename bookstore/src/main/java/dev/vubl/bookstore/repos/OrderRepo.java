84cb807c4e01ad6099028a334f28c742e905e45d
B5u2423 <masterofyordle@gmail.com>
2025-12-31 10:31:32 +0700

    Get user order history endpoints

--- bookstore/src/main/java/dev/vubl/bookstore/repos/OrderRepo.java
+++ bookstore/src/main/java/dev/vubl/bookstore/repos/OrderRepo.java
@@ -1,8 +1,12 @@
 package dev.vubl.bookstore.repos;
 
 import dev.vubl.bookstore.entities.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepo extends JpaRepository<Order, Integer> {}
+public interface OrderRepo extends JpaRepository<Order, Integer> {
+  Page<Order> findAllByEmail(String email, Pageable pageable);
+}
