5510372989eaec1ab94e6f110c721d6ebc883a4c
B5u2423 <masterofyordle@gmail.com>
2026-06-08 21:47:52 +0700

    fix: add vuetify vite plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,19 +3,16 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
 import '@mdi/font/css/materialdesignicons.css'
+
+import { createVuetify } from 'vuetify'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 const vuetify = createVuetify({
-  components,
-  directives,
   icons: {
     defaultSet: 'mdi',
   },
