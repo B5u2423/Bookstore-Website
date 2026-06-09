2e2485b94dc6c52cd7e65af7dd3c7d9793c92d19
B5u2423 <masterofyordle@gmail.com>
2026-06-10 00:40:08 +0700

    new: common chart components

---
+++ frontend/src/components/charts/DoughnutChart.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Doughnut } from 'vue-chartjs'
+
+const props = defineProps({
+  data: Object,
+  options: Object,
+})
+</script>
+
+<template>
+    <Doughnut :data="data" :options="options" />
+</template>
---
+++ frontend/src/components/charts/LineChart.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Line } from 'vue-chartjs'
+
+const props = defineProps({
+  data: Object,
+  options: Object,
+})
+</script>
+
+<template>
+    <Line :data="data" :options="options" />
+</template>
