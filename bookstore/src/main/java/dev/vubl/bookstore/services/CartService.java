e3b7ac92bef7f68d2d377f05f70576827b151cb8
B5u2423 <masterofyordle@gmail.com>
2026-06-18 00:25:07 +0700

    misc: rewrite cart service and api
    where do I begin:
    - now sync cart with a single api call instead of multiple requests.
    - modular helper methods
    - more logs (yay)

--- bookstore/src/main/java/dev/vubl/bookstore/services/CartService.java
+++ bookstore/src/main/java/dev/vubl/bookstore/services/CartService.java
@@ -1,13 +1,16 @@
 package dev.vubl.bookstore.services;
 
 import dev.vubl.bookstore.dtos.AddToCartRequest;
+import dev.vubl.bookstore.dtos.CartDto;
 import dev.vubl.bookstore.entities.*;
 import dev.vubl.bookstore.exceptions.BookDoesNotExistException;
 import dev.vubl.bookstore.exceptions.UserDoesNotExistException;
+import dev.vubl.bookstore.mappers.CartMapper;
 import dev.vubl.bookstore.repos.BookRepo;
 import dev.vubl.bookstore.repos.CartItemRepo;
 import dev.vubl.bookstore.repos.CartRepo;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,19 +27,9 @@ public class CartService {
   private final AuthService authService;
 
   public void addToCart(AddToCartRequest payload, String token) {
-
     // find user's active cart or create new one
     Cart cart = getActiveCartByUser(token);
-    Book book = bookRepo.findById(payload.bookId()).orElseThrow(BookDoesNotExistException::new);
-
-    Optional<CartItem> ci = cartItemRepo.findByBookAndCart(book, cart);
-    if (ci.isPresent()) {
-      ci.get().setQuantity(ci.get().getQuantity() + payload.quantity());
-    } else {
-      log.info("[{}] New item added to cart", this.getClass().getName());
-      cartItemRepo.save(
-          CartItem.builder().cart(cart).book(book).quantity(payload.quantity()).build());
-    }
+    addItemToUserCartHelper(cart, payload);
   }
 
   public Cart getActiveCartByUser(String token) {
@@ -50,7 +43,33 @@ public class CartService {
         () -> cartRepo.save(Cart.builder().cartStatus(CartStatus.ACTIVE).user(user).build()));
   }
 
-  public void removeAllItems(String token) {
+  public CartDto getActiveUserCartToDto(String token) {
+    return CartMapper.INSTANCE.toDto(getActiveCartByUser(token));
+  }
+
+  public void syncCart(String token, List<AddToCartRequest> payload) {
+    log.info("Start removing all items in cart");
+    removeAllItemsHelper(token);
+    log.info("Start re-adding all items in cart");
+    Cart cart = getActiveCartByUser(token);
+    payload.forEach(r -> addItemToUserCartHelper(cart, r));
+    log.info("[{}] Sync cart items successfully", this.getClass().getName());
+  }
+
+  private void addItemToUserCartHelper(Cart c, AddToCartRequest r) {
+    Book book = bookRepo.findById(r.bookId()).orElseThrow(BookDoesNotExistException::new);
+    Optional<CartItem> ci = cartItemRepo.findByBookAndCart(book, c);
+    if (ci.isPresent()) {
+      ci.get().setQuantity(ci.get().getQuantity() + r.quantity());
+      log.info(
+          "CartItemId {} increase by {}, in cartId {}", ci.get().getId(), r.quantity(), c.getId());
+    } else {
+      cartItemRepo.save(CartItem.builder().cart(c).book(book).quantity(r.quantity()).build());
+      log.info("New item added to cart with id {}", c.getId());
+    }
+  }
+
+  private void removeAllItemsHelper(String token) {
     try {
       Cart c = getActiveCartByUser(token);
       cartItemRepo.deleteByCart(c);
