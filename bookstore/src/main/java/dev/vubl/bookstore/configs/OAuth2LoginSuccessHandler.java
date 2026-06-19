401cbf42a6c5517f1d9436ad35be2079898b732a
B5u2423 <masterofyordle@gmail.com>
2026-06-19 22:38:33 +0700

    fix: refactor oauth2 service
    move the core logic into a service instead of doing it in the handler.

--- bookstore/src/main/java/dev/vubl/bookstore/configs/OAuth2LoginSuccessHandler.java
+++ bookstore/src/main/java/dev/vubl/bookstore/configs/OAuth2LoginSuccessHandler.java
@@ -3,15 +3,11 @@ package dev.vubl.bookstore.configs;
 import static dev.vubl.bookstore.utils.Constants.*;
 
 import dev.vubl.bookstore.dtos.LoginResponse;
-import dev.vubl.bookstore.dtos.RegistrationRequest;
-import dev.vubl.bookstore.entities.UserType;
 import dev.vubl.bookstore.services.ApplicationUserService;
 import dev.vubl.bookstore.services.AuthService;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -32,43 +28,16 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
   @Override
   public void onAuthenticationSuccess(
       HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-      throws IOException, ServletException {
+      throws IOException {
+    if (!(authentication instanceof OAuth2AuthenticationToken oauthToken)) {
+      throw new IllegalArgumentException("Unexpected authentication type");
+    }
+
     OAuth2User user = (OAuth2User) authentication.getPrincipal();
-    OAuth2AuthenticationToken oAuth2AuthenticationToken =
-        (OAuth2AuthenticationToken) authentication;
 
-    // get provider: google or facebook
-    String registrationId = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
-    String email, name, id;
-
-    switch (registrationId) {
-      case IDP_GOOGLE -> {
-        email = user.getAttribute(ATTR_EMAIL);
-        name = user.getAttribute(ATTR_USERNAME);
-        id = user.getAttribute(ATTR_GOOGLE_ID);
-      }
-      case IDP_FACEBOOK -> {
-        email = user.getAttribute(ATTR_EMAIL);
-        name = user.getAttribute(ATTR_USERNAME);
-        id = user.getAttribute(ATTR_FACEBOOK_ID);
-      }
-      default -> throw new IllegalStateException("Invalid provider!");
-    }
-    // register if it does not exist
-    if (!userService.isUserExistByEmail(email)) {
-      authService.registerUser(
-          RegistrationRequest.builder()
-              .userType(UserType.CUSTOMER)
-              .email(email)
-              .name(name)
-              .password(UUID.randomUUID().toString())
-              .build(),
-          registrationId,
-          id);
-    }
+    String registrationId = oauthToken.getAuthorizedClientRegistrationId();
     // login
-    LoginResponse res = authService.logInOAuth(email);
-
+    LoginResponse res = authService.logInOAuth(user, registrationId);
     // redirect
     String redirectUrl =
         "%s/oauth2/callback?token=%s&ref=%s".formatted(frontEndUrl, res.token(), res.refresh());
--- bookstore/src/main/java/dev/vubl/bookstore/services/AuthService.java
+++ bookstore/src/main/java/dev/vubl/bookstore/services/AuthService.java
@@ -1,20 +1,26 @@
 package dev.vubl.bookstore.services;
 
-import static dev.vubl.bookstore.utils.Constants.IDP_FACEBOOK;
-import static dev.vubl.bookstore.utils.Constants.IDP_GOOGLE;
+import static dev.vubl.bookstore.utils.Constants.*;
+import static dev.vubl.bookstore.utils.Constants.ATTR_EMAIL;
+import static dev.vubl.bookstore.utils.Constants.ATTR_FACEBOOK_ID;
+import static dev.vubl.bookstore.utils.Constants.ATTR_GOOGLE_ID;
+import static dev.vubl.bookstore.utils.Constants.ATTR_USERNAME;
 
 import dev.vubl.bookstore.dtos.*;
 import dev.vubl.bookstore.entities.ApplicationUser;
+import dev.vubl.bookstore.entities.UserType;
 import dev.vubl.bookstore.exceptions.InvalidCredentialException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -69,16 +75,44 @@ public class AuthService {
     tokenService.deleteRefreshTokenByUser(user);
   }
 
-  public LoginResponse logInOAuth(String email) {
+  public LoginResponse logInOAuth(OAuth2User user, String registrationId) {
     try {
-      ApplicationUser user = userService.readUserByEmail(email);
+      String email, name, id;
+
+      // get provider: google or facebook
+      email = user.getAttribute(ATTR_EMAIL);
+      name = user.getAttribute(ATTR_USERNAME);
+      switch (registrationId) {
+        case IDP_GOOGLE -> {
+          id = user.getAttribute(ATTR_GOOGLE_ID);
+        }
+        case IDP_FACEBOOK -> {
+          id = user.getAttribute(ATTR_FACEBOOK_ID);
+        }
+        default -> throw new IllegalStateException("Invalid provider!");
+      }
+
+      // registration if user does not exist
+      if (!userService.isUserExistByEmail(email)) {
+        registerUser(
+            RegistrationRequest.builder()
+                .userType(UserType.CUSTOMER)
+                .email(email)
+                .name(name)
+                .password(UUID.randomUUID().toString())
+                .build(),
+            registrationId,
+            id);
+      }
+      ApplicationUser au = userService.readUserByEmail(email);
       // generate access token
-      String jwtToken = tokenService.generateJwt(user);
+      String jwtToken = tokenService.generateJwt(au);
       // generate refresh token
-      String refreshToken = tokenService.generateRefreshToken(user).getRefreshToken();
+      String refreshToken = tokenService.generateRefreshToken(au).getRefreshToken();
 
       return LoginResponse.builder().token(jwtToken).refresh(refreshToken).build();
     } catch (Exception e) {
+      log.error("Error log in/register via OAuth2.0");
       throw new IllegalArgumentException("Some thing is wrong with OAuth y'know...");
     }
   }
